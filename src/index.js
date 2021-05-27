fab93db361963435fc2dbc1d00c165a539425c62
prodanserega <74560554+prodanserega@users.noreply.github.com>
2021-05-27 17:00:58 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,20 +11,20 @@ galleryRef.insertAdjacentHTML('beforeend', markUp)
 
 
 const Theme = {
-    LIGHT: 'light-theme',
-    DARK: 'dark-theme',
-  };
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
 
-const currentTheme = localStorage.getItem('theme');
+const localTheme = localStorage.getItem('theme');
 const themeToggle = document.querySelector('#theme-switch-toggle');
 
 themeToggle.addEventListener('change', onToggleChange);
 
-if (currentTheme) {
-  document.body.classList.add(currentTheme);
+if (localTheme) {
+  document.body.classList.add(localTheme);
 }
 
-if (currentTheme === Theme.DARK) {
+if (localTheme === Theme.DARK) {
   themeToggle.checked = true;
 }
 
