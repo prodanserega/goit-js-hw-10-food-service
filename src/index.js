f68bda6a91e23f8ca991ca53d10e17ab0d574495
prodanserega <74560554+prodanserega@users.noreply.github.com>
2021-05-27 15:52:38 +0300

    1

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import './sass/main.scss';
+import './styles.css';
+import menuGalerry from './menu.json';
+import itemsMenu from './tamplates/menu.hbs';
+
+
+const galleryRef = document.querySelector('.js-menu')
+
+const markUp = itemsMenu(menuGalerry)
+galleryRef.insertAdjacentHTML('beforeend', markUp)
